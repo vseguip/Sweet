449b2ae8d48b6a677fc3b48a3a683b36a8071c18
Vicent Seguí Pascual <vseguip@gmail.com>
2011-04-09 10:57:35 +0200

    The beginning of Sweet
    Signed-off-by: Vicent Seguí Pascual <vseguip@gmail.com>

---
+++ src/com/github/vseguip/sweet/SweetAccountManager.java
@@ -0,0 +1,13 @@
+package com.github.vseguip.sweet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SweetAccountManager extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
