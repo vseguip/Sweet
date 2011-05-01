33d95a1473c26ae2ed3c0903ab98cea1ce216150
Vicent Seguí Pascual <vseguip@gmail.com>
2011-05-01 21:35:54 +0200

    Cleanup imports
    Signed-off-by: Vicent Seguí Pascual <vseguip@gmail.com>

--- src/com/github/vseguip/sweet/rest/SugarRestAPI.java
+++ src/com/github/vseguip/sweet/rest/SugarRestAPI.java
@@ -30,7 +30,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
