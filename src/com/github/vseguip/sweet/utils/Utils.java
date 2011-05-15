9e25fff5103ee689b0ba7c57e089d2198e0266da
Vicent Seguí Pascual <vseguip@gmail.com>
2011-05-15 11:41:35 +0200

    - Add simple method to get boolean data from an account
    Signed-off-by: Vicent Seguí Pascual <vseguip@gmail.com>

---
+++ src/com/github/vseguip/sweet/utils/Utils.java
@@ -0,0 +1,43 @@
+/********************************************************************\
+
+File: Utils.java
+Copyright 2011 Vicent Seguí Pascual
+
+This file is part of Sweet.  Sweet is free software: you can
+redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+Sweet is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.  You should have received a copy of the GNU General
+Public License along with Sweet. 
+
+If not, see http://www.gnu.org/licenses/.  
+\********************************************************************/
+
+package com.github.vseguip.sweet.utils;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+
+public class Utils {
+	public static boolean getBooleanAccountData(AccountManager am, Account account, String key, boolean defaultValue) {
+		boolean value = defaultValue;
+		try {
+			String data = am.getUserData(account, key);
+			if (data != null)
+				value = Boolean.parseBoolean(data);
+			else
+				am.setUserData(account, key, Boolean.toString(defaultValue));
+		} catch (Exception ex) {
+			try {
+				am.setUserData(account, key, Boolean.toString(defaultValue));
+			} catch (Exception ex2) {
+
+			}
+		}
+		return value;
+	}
+}
